4fcbfb8b62f4c681de9b3f45fc5a7210263cc5aa
suddiyo <suddiyo@naver.com>
2024-01-04 01:28:01 +0900

    [Feat/#1] 프로젝트 생성

---
+++ src/test/java/com/friends/easybud/EasybudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friends.easybud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
