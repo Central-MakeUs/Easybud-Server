5315033b75b16e0b48493699555de8de565b4caa
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-07 15:38:42 +0900

    [Feat/#7] BaseTimeEntity 작성

--- src/main/java/com/friends/easybud/EasybudApplication.java
+++ src/main/java/com/friends/easybud/EasybudApplication.java
@@ -2,12 +2,14 @@ package com.friends.easybud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class EasybudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EasybudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EasybudApplication.class, args);
+    }
 
 }
