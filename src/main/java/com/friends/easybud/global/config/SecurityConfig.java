14954a0b0b637e3ec0e5e1042dadafd81234e8d7
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-08 12:38:32 +0900

    [Feat/#7] SecurityConfig 작성

---
+++ src/main/java/com/friends/easybud/global/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.friends.easybud.global.config;
+
+import com.friends.easybud.auth.handler.OAuth2AuthenticationSuccessHandler;
+import com.friends.easybud.auth.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        (authorizeRequests) -> authorizeRequests
+                                .requestMatchers(
+                                        "/", "/swagger-ui/**", "/oauth2/**", "auth/**",
+                                        "/swagger-resources/**", "/v3/api-docs/**").permitAll()
+                                .anyRequest().hasRole("USER")
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .userInfoEndpoint(userInfoEndpointConfig ->
+                                        userInfoEndpointConfig.userService(customOAuth2UserService))
+                                .redirectionEndpoint(config ->
+                                        config.baseUri("/oauth/callback/kakao"))
+                                .successHandler(oAuth2AuthenticationSuccessHandler)
+                )
+                .build();
+    }
+
+}
