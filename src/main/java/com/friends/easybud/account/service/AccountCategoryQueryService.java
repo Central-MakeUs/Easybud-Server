3d022dd254cb84325b59feca39813ae792c8dbe1
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-20 01:32:20 +0900

    [Feat/#13] 계정 카테고리 서비스 로직 구현

---
+++ src/main/java/com/friends/easybud/account/service/AccountCategoryQueryService.java
@@ -0,0 +1,10 @@
+package com.friends.easybud.account.service;
+
+import com.friends.easybud.account.domain.AccountCategory;
+import java.util.List;
+
+public interface AccountCategoryQueryService {
+
+    List<AccountCategory> getAccountCategories();
+
+}
--- src/main/java/com/friends/easybud/global/response/code/ErrorStatus.java
+++ src/main/java/com/friends/easybud/global/response/code/ErrorStatus.java
@@ -3,6 +3,7 @@ package com.friends.easybud.global.response.code;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import lombok.AllArgsConstructor;
@@ -17,8 +18,13 @@ public enum ErrorStatus implements BaseCode {
     _INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, 5000, "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(BAD_REQUEST, 4000, "잘못된 요청입니다."),
     _UNAUTHORIZED(UNAUTHORIZED, 4001, "로그인이 필요합니다."),
-    _FORBIDDEN(FORBIDDEN, 4002, "금지된 요청입니다.");
+    _FORBIDDEN(FORBIDDEN, 4002, "금지된 요청입니다."),
 
+    // Member: 4050 ~ 4099
+    MEMBER_NOT_FOUND(NOT_FOUND, 4050, "존재하지 않는 회원입니다."),
+
+    // AccountCategory: 4100 ~ 4149
+    ACCOUNT_CATEGORY_NOT_FOUND(NOT_FOUND, 4100, "존재하지 않는 계정 카테고리입니다.");
 
     private final HttpStatus httpStatus;
     private final Integer code;
