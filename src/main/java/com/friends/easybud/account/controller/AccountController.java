327093489ae8bda63245e2aa905d637578f6ae26
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-22 20:08:21 +0900

    [Feat/#21] 계정 API 설계

---
+++ src/main/java/com/friends/easybud/account/controller/AccountController.java
@@ -0,0 +1,36 @@
+package com.friends.easybud.account.controller;
+
+import static com.friends.easybud.account.converter.AccountRequest.AccountCreateDto;
+
+import com.friends.easybud.global.response.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/v1/accounts")
+@RestController
+@Tag(name = "Account API", description = "계정 API")
+public class AccountController {
+
+    @Operation(summary = "계정 생성", description = "새로운 계정을 생성합니다.")
+    @PostMapping
+    public ResponseDto<Long> createAccount(@RequestBody AccountCreateDto request) {
+        return ResponseDto.onSuccess(1L);
+    }
+
+    @Operation(summary = "계정 삭제", description = "기존의 계정을 삭제합니다.")
+    @Parameter(name = "accountId", description = "삭제할 계정의 ID")
+    @DeleteMapping
+    public ResponseDto<Boolean> deleteAccount(@RequestParam Long accountId) {
+        return ResponseDto.onSuccess(Boolean.TRUE);
+    }
+
+}
