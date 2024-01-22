f41f405b93fbbc096f578714946d4199701a6e8c
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-22 21:04:52 +0900

    [Feat/#21] 계정 생성, 삭제 API 구현

--- src/main/java/com/friends/easybud/account/controller/AccountController.java
+++ src/main/java/com/friends/easybud/account/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.friends.easybud.account.controller;
 
 import com.friends.easybud.account.dto.AccountRequest.AccountWithCardCreateDto;
 import com.friends.easybud.account.dto.AccountRequest.AccountWithTertiaryCategoryCreateDto;
+import com.friends.easybud.account.service.AccountCommandService;
 import com.friends.easybud.global.response.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -20,24 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Account API", description = "계정 API")
 public class AccountController {
 
+    private final AccountCommandService accountCommandService;
+
     @Operation(summary = "계정 생성 (카드)", description = "카드 정보가 포함된 새로운 계정을 생성합니다.")
     @PostMapping
     public ResponseDto<Long> createAccountWithCard(@RequestBody AccountWithCardCreateDto request) {
-        return ResponseDto.onSuccess(1L);
+        return ResponseDto.onSuccess(accountCommandService.createAccountWithCard(request));
     }
 
     @Operation(summary = "계정 생성 (소분류)", description = "소분류가 포함된 새로운 계정을 생성합니다.")
     @PostMapping
     public ResponseDto<Long> createAccountWithTertiaryCategory(
             @RequestBody AccountWithTertiaryCategoryCreateDto request) {
-        return ResponseDto.onSuccess(1L);
+        return ResponseDto.onSuccess(accountCommandService.createAccountWithTertiaryCategory(request));
     }
 
     @Operation(summary = "계정 삭제", description = "기존의 계정을 삭제합니다.")
     @Parameter(name = "accountId", description = "삭제할 계정의 ID")
     @DeleteMapping
     public ResponseDto<Boolean> deleteAccount(@RequestParam Long accountId) {
-        return ResponseDto.onSuccess(Boolean.TRUE);
+        return ResponseDto.onSuccess(accountCommandService.deleteAccount(accountId));
     }
 
 }
