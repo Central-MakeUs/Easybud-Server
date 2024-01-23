e605afdfc9e96d322224deb235c9ec2d879abd61
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-24 01:37:41 +0800

    [Feat/#33] 거래 생성과 연결된 계정 처리 로직 구현

--- src/main/java/com/friends/easybud/transaction/dto/TransactionRequest.java
+++ src/main/java/com/friends/easybud/transaction/dto/TransactionRequest.java
@@ -1,8 +1,43 @@
 package com.friends.easybud.transaction.dto;
 
+import com.friends.easybud.account.domain.AccountType;
+import com.friends.easybud.transaction.domain.TransactionType;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 public class TransactionRequest {
 
+    @Builder
+    @Setter
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class TransactionCreateDto {
-
+        private LocalDateTime date;
+        private String summary;
+        private TransactionType type;
+        private List<AccountCreateDto> accounts;
     }
+
+    @Builder
+    @Setter
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AccountCreateDto {
+        private AccountType accountType;
+        private BigDecimal amount;
+        private Long tertiaryCategoryId;
+        private Long cardId;
+    }
+
 }
