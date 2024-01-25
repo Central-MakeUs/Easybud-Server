a0233b70d2df6b0c75c3779a8330e60729916a9c
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-26 01:21:53 +0800

    [Feat/#43] application.yml에 토큰, 채널명 등록

---
+++ src/main/java/com/friends/easybud/global/slack/SlackService.java
@@ -0,0 +1,43 @@
+package com.friends.easybud.slack;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.model.Attachment;
+import com.slack.api.model.block.LayoutBlock;
+import java.io.IOException;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SlackService {
+
+    @Value(value = "${spring.profiles.active}")
+    String profile;
+    @Value(value = "${slack.token}")
+    String token;
+    @Value(value = "${slack.channel.monitor}")
+    String channelProductError;
+
+    private static final String LOCAL = "local";
+    private static final String PROD_ERROR_MESSAGE_TITLE = "🤯 *500 에러 발생*";
+    private static final String ATTACHMENTS_ERROR_COLOR = "#eb4034";
+
+    public void sendSlackMessageProductError(Exception exception) {
+        if (!profile.equals(LOCAL)) {
+            try {
+                List<LayoutBlock> layoutBlocks = SlackServiceUtils.createProdErrorMessage(exception);
+                List<Attachment> attachments = SlackServiceUtils.createAttachments(ATTACHMENTS_ERROR_COLOR,
+                        layoutBlocks);
+                Slack.getInstance().methods(token).chatPostMessage(request ->
+                        request.channel(channelProductError)
+                                .attachments(attachments)
+                                .text(PROD_ERROR_MESSAGE_TITLE));
+            } catch (SlackApiException | IOException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/friends/easybud/global/slack/SlackServiceUtils.java
@@ -0,0 +1,54 @@
+package com.friends.easybud.slack;
+
+import static com.slack.api.model.block.Blocks.divider;
+import static com.slack.api.model.block.Blocks.section;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+
+import com.slack.api.model.Attachment;
+import com.slack.api.model.block.LayoutBlock;
+import com.slack.api.model.block.composition.TextObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlackServiceUtils {
+
+    private static final String ERROR_MESSAGE = "*Error Message:*\n";
+    private static final String ERROR_STACK = "*Error Stack:*\n";
+    private static final String FILTER_STRING = "blossom";
+
+    public static List<Attachment> createAttachments(String color, List<LayoutBlock> data) {
+        List<Attachment> attachments = new ArrayList<>();
+        Attachment attachment = new Attachment();
+        attachment.setColor(color);
+        attachment.setBlocks(data);
+        attachments.add(attachment);
+        return attachments;
+    }
+
+    public static List<LayoutBlock> createProdErrorMessage(Exception exception) {
+        StackTraceElement[] stacks = exception.getStackTrace();
+
+        List<LayoutBlock> layoutBlockList = new ArrayList<>();
+
+        List<TextObject> sectionInFields = new ArrayList<>();
+        sectionInFields.add(markdownText(ERROR_MESSAGE + exception.getMessage()));
+        sectionInFields.add(markdownText(ERROR_STACK + exception));
+        layoutBlockList.add(section(section -> section.fields(sectionInFields)));
+
+        layoutBlockList.add(divider());
+        layoutBlockList.add(section(section -> section.text(markdownText(filterErrorStack(stacks)))));
+        return layoutBlockList;
+    }
+
+    private static String filterErrorStack(StackTraceElement[] stacks) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("```");
+        for (StackTraceElement stack : stacks) {
+            if (stack.toString().contains(FILTER_STRING)) {
+                stringBuilder.append(stack).append("\n");
+            }
+        }
+        stringBuilder.append("```");
+        return stringBuilder.toString();
+    }
+}
