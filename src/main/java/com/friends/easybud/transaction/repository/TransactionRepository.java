4881ffc16305e9a66ffd6d074b259c68edab772e
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-25 23:56:29 +0800

    [Feat/#40] 특정 회원 거래 조회를 위한 거래 API 수정

--- src/main/java/com/friends/easybud/transaction/repository/TransactionRepository.java
+++ src/main/java/com/friends/easybud/transaction/repository/TransactionRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    List<Transaction> findByDateBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    List<Transaction> findByMemberIdAndDateBetween(Long memberId, LocalDateTime startDateTime,
+                                                   LocalDateTime endDateTime);
 
-    List<Transaction> findTop3ByOrderByDateDesc();
+    List<Transaction> findTop3ByMemberIdOrderByDateDesc(Long memberId);
 
 }
