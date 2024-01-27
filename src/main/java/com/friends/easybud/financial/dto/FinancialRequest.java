3b498207b3ba9797f5a17dbb64c749783cb90c82
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-27 17:03:14 +0900

    [Feat/#42] Financial DTO example 작성

--- src/main/java/com/friends/easybud/financial/dto/FinancialRequest.java
+++ src/main/java/com/friends/easybud/financial/dto/FinancialRequest.java
@@ -20,10 +20,10 @@ public class FinancialRequest {
     @Schema(description = "장부 날짜 DTO")
     public static class FinancialDateDto {
 
-        @Schema(description = "시작 날짜")
+        @Schema(description = "시작 날짜", example = "2024-01-01")
         private LocalDate startDate;
 
-        @Schema(description = "종료 날짜")
+        @Schema(description = "종료 날짜", example = "2024-01-27")
         private LocalDate endDate;
 
     }
