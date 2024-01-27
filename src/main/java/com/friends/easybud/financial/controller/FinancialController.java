18403acfbe4d6e94b561dae3be248c588eccfea7
ahnsugyeong <ahnsugyeong@gmail.com>
2024-01-27 16:41:26 +0900

    [Feat/#42] 장부 조회 API POST -> GET 변경

--- src/main/java/com/friends/easybud/financial/controller/FinancialController.java
+++ src/main/java/com/friends/easybud/financial/controller/FinancialController.java
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,19 +25,19 @@ public class FinancialController {
     private final FinancialService financialService;
 
     @Operation(summary = "가용자금 조회", description = "사용자의 가용자금을 조회합니다.")
-    @PostMapping("/available-funds")
+    @GetMapping("/available-funds")
     public ResponseDto<AvailableFundsDto> getAvailableFunds() {
         return ResponseDto.onSuccess(financialService.getAvailableFunds());
     }
 
     @Operation(summary = "재무 상태 조회", description = "사용자의 재무 상태를 조회합니다.")
-    @PostMapping("/financial-statement")
+    @GetMapping("/financial-statement")
     public ResponseDto<FinancialStatementDto> getFinancialStatement() {
         return ResponseDto.onSuccess(financialService.getFinancialStatement());
     }
 
     @Operation(summary = "손익현황 조회", description = "사용자의 손익현황을 조회합니다.")
-    @PostMapping("/income-statement")
+    @GetMapping("/income-statement")
     public ResponseDto<IncomeStatementDto> getIncomeStatement(@RequestBody FinancialDateDto request) {
         LocalDateTime startOfDay = request.getStartDate().atStartOfDay();
         LocalDateTime endOfDay = request.getEndDate().atTime(23, 59, 59);
