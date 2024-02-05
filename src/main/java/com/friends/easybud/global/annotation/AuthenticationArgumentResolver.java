55380a4df206d19d1c4b9c99ae6011f415c060c9
ahnsugyeong <ahnsugyeong@gmail.com>
2024-02-05 23:27:07 +0900

    [Feat/#85] 커스텀 어노테이션 @AuthUser 작성

---
+++ src/main/java/com/friends/easybud/global/annotation/AuthenticationArgumentResolver.java
@@ -0,0 +1,46 @@
+package com.friends.easybud.global.annotation;
+
+import com.friends.easybud.global.exception.GeneralException;
+import com.friends.easybud.global.response.code.ErrorStatus;
+import com.friends.easybud.jwt.JwtProvider;
+import com.friends.easybud.member.domain.Member;
+import com.friends.easybud.member.service.MemberQueryService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class AuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider jwtProvider;
+    private final MemberQueryService memberQueryService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        final boolean isRegUserAnnotation = parameter.getParameterAnnotation(Authuser.class) != null;
+        final boolean isMember = parameter.getParameterType().equals(Member.class);
+        return isRegUserAnnotation && isMember;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+            String token = jwtProvider.resolveToken(request);
+            String uid = jwtProvider.getAuthentication(token).getName();
+            return memberQueryService.getMemberByUid(uid);
+        } else {
+            throw new GeneralException(ErrorStatus.TOKEN_INVALID);
+        }
+    }
+}
